836b05626a512d777572c2b3caa13e4423dfadb2
chenchenYellow <yuchenhuang97@gmail.com>
2019-03-21 21:02:09 -0400

    Update Activity.aspx.designer.cs

--- Activity.aspx.designer.cs
+++ Activity.aspx.designer.cs
@@ -191,5 +191,14 @@ namespace FriendBook {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel messagePanel;
+        
+        /// <summary>
+        /// lblTest control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTest;
     }
 }
